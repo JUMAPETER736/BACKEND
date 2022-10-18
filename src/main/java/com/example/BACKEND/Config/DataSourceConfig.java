5fa628ec347f753b2bfb14ade647abb596049487
Peter Juma <bed-com-47-19@unima.ac.mw>
2022-10-18 19:15:49 +0200

    Creating Database and some conections

---
+++ src/main/java/com/example/BACKEND/Config/DataSourceConfig.java
@@ -0,0 +1,18 @@
+package com.example.BACKEND.Config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties
+
+
+
+}
--- src/main/java/com/example/BACKEND/appuser/AppUserRole.java
+++ src/main/java/com/example/BACKEND/appuser/AppUserRole.java
@@ -3,5 +3,5 @@ package com.example.BACKEND.appuser;
 public enum AppUserRole {
     USER,
     ADMIN
-    
+
 }
