02729ae2429194e638957676f034e9b1f0c6489e
Peter Juma <bed-com-47-19@unima.ac.mw>
2022-10-25 08:23:41 +0200

    Creating V1 Migration

--- src/main/resources/db/migration/V1__initialTable.sql
+++ src/main/resources/db/migration/V1__initialTable.sql
@@ -1,13 +1,13 @@
 
 
-CREATE TABLE login(
-
-id  BIGSERIAL,
-username VARCHAR(255) not null,
-email varchar(255) not null,
-password varchar(255)not null,
-
-primary key(id),
-unique (id)
-
-)
+--CREATE TABLE login(
+--
+--id  BIGSERIAL,
+--username VARCHAR(255) not null,
+--email varchar(255) not null,
+--password varchar(255)not null,
+--
+--primary key(id),
+--unique (id)
+--
+--)
